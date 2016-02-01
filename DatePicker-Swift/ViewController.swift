3f7625b72746eb48127f24a268b079c60dc40bd2
尹彩霞 <meryincx@163.com>
2016-02-01 10:21:59 +0800

    init

--- DatePicker-Swift/ViewController.swift
+++ DatePicker-Swift/ViewController.swift
@@ -9,15 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timeLabel:UILabel?
+    var picker:DatePickerView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.whiteColor()
+        let btn:UIButton = UIButton(type: UIButtonType.Custom)
+        btn.frame = CGRectMake(20, 100, 100, 100);
+        btn.setTitle("选择日期", forState: UIControlState.Normal)
+        btn.addTarget(self, action: Selector("click:"), forControlEvents: UIControlEvents.TouchUpInside)
+        btn.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        self.view.addSubview(btn)
+        timeLabel = UILabel(frame: CGRectMake(20, 150, 300, 100))
+        self.view.addSubview(timeLabel!)
+        timeLabel?.textColor = UIColor.blackColor()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func click(sender: UIButton)
+    {
+        picker = DatePickerView.getShareInstance()
+        picker!.textColor = UIColor.redColor()
+        picker!.showWithDate(NSDate())
+        picker?.block = {
+            (date:NSDate)->() in
+            let formatter = NSDateFormatter()
+            formatter.dateFormat = "yyyy年-MM月-dd日 zzz"
+            let string = formatter.stringFromDate(date)
+            let range:Range = string.rangeOfString(" ")!
+            let time = string.substringToIndex(range.endIndex)
+            self.timeLabel?.text = time
+        }
     }
 
 
